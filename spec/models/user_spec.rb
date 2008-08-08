cfb2990a2153b100006f70cd1dfb9c1d78c89b0e
Eric Allen <ericpallen@gmail.com>
2008-08-08 11:18:11 -0700

    More speccage for acts_as_tester

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,4 +29,20 @@ describe User do
     
     SplitTesting::FeatureTester.find(:first, :conditions => {:feature_id => feat.id, :tester_id => @user.id}).should be_enabled
   end
+
+  it "should allow disabling after enabling" do
+    feat = SplitTesting::Feature.generate(:name => 'foo')
+    @user.enable_tester(:foo)
+    @user.disable_tester(:foo)
+
+    SplitTesting::FeatureTester.find(:first, :conditions => {:feature_id => feat.id, :tester_id => @user.id}).should_not be_enabled
+  end
+
+  it "should allow enabling after disable_tester" do
+    feat = SplitTesting::Feature.generate(:name => 'foo')
+    @user.disable_tester(:foo)
+    @user.enable_tester(:foo)
+
+    SplitTesting::FeatureTester.find(:first, :conditions => {:feature_id => feat.id, :tester_id => @user.id}).should be_enabled
+  end
 end
